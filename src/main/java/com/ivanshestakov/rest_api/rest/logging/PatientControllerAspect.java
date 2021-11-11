abd7bb9109e23964cd9c5cfcb241128a8d473363
VanyaShestakov <vanka1108@gmail.com>
2021-11-11 19:55:30 +0300

    Configured logging for all methods in PatientController.java

--- src/main/java/com/ivanshestakov/rest_api/rest/logging/PatientControllerAspect.java
+++ src/main/java/com/ivanshestakov/rest_api/rest/logging/PatientControllerAspect.java
@@ -8,18 +8,11 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.lang.annotation.Annotation;
+import org.springframework.web.bind.annotation.*;
 import java.lang.reflect.Method;
-import java.net.http.HttpRequest;
 import java.util.Arrays;
 import java.util.Optional;
-
 
 @Component
 @Aspect
@@ -27,16 +20,16 @@ public class PatientControllerAspect {
     private final static Logger log = LogManager.getLogger(PatientController.class);
 
     @Pointcut("execution(*  com.ivanshestakov.rest_api.rest.controller.PatientController.get*(..))")
-    private void getControllerMethods(){};
+    private void getControllerMethods(){}
 
     @Pointcut("execution(*  com.ivanshestakov.rest_api.rest.controller.PatientController.post*(..))")
-    private void postControllerMethods(){};
+    private void postControllerMethods(){}
 
     @Pointcut("execution(*  com.ivanshestakov.rest_api.rest.controller.PatientController.put*(..))")
-    private void putControllerMethods(){};
+    private void putControllerMethods(){}
 
     @Pointcut("execution(*  com.ivanshestakov.rest_api.rest.controller.PatientController.delete*(..))")
-    private void deleteControllerMethods(){};
+    private void deleteControllerMethods(){}
 
 
     @Around("getControllerMethods()")
@@ -44,17 +37,12 @@ public class PatientControllerAspect {
         MethodSignature signature = (MethodSignature) jp.getSignature();
         Method method = signature.getMethod();
         Optional<GetMapping> getMappingOptional = Optional.ofNullable(method.getAnnotation(GetMapping.class));
-        getMappingOptional.ifPresent(ann -> {
-            addLogInfo("GET", ann.value(), method.getName());
-        });
+        getMappingOptional.ifPresent(ann -> addLogInfo("GET", ann.value(), method.getName()));
         Object obj;
         try {
             obj = jp.proceed();
         } catch (Exception e) {
-            log.warn(e.getClass().getSimpleName() +
-                    " was thrown with message: " +
-                    e.getMessage() +
-                    " in method " + method.getName());
+            addLogException(e, method);
             throw e;
         }
         return obj;
@@ -65,17 +53,44 @@ public class PatientControllerAspect {
         MethodSignature signature = (MethodSignature) jp.getSignature();
         Method method = signature.getMethod();
         Optional<PostMapping> postMappingOptional = Optional.ofNullable(method.getAnnotation(PostMapping.class));
-        postMappingOptional.ifPresent(ann -> {
-            addLogInfo("POST", ann.value(), method.getName());
-        });
+        postMappingOptional.ifPresent(ann -> addLogInfo("POST", ann.value(), method.getName()));
         Object obj;
         try {
             obj = jp.proceed();
         } catch (Exception e) {
-            log.warn(e.getClass().getSimpleName() +
-                    " was thrown with message: " +
-                    e.getMessage() +
-                    " in method " + method.getName());
+            addLogException(e, method);
+            throw e;
+        }
+        return obj;
+    }
+
+    @Around("putControllerMethods()")
+    private Object aroundPutControllerMethodsAdvice(ProceedingJoinPoint jp) throws Throwable {
+        MethodSignature signature = (MethodSignature) jp.getSignature();
+        Method method = signature.getMethod();
+        Optional<PutMapping> putMappingOptional = Optional.ofNullable(method.getAnnotation(PutMapping.class));
+        putMappingOptional.ifPresent(ann -> addLogInfo("PUT", ann.value(), method.getName()));
+        Object obj;
+        try {
+            obj = jp.proceed();
+        } catch (Exception e) {
+            addLogException(e, method);
+            throw e;
+        }
+        return obj;
+    }
+
+    @Around("deleteControllerMethods()")
+    private Object aroundDeleteControllerMethodsAdvice(ProceedingJoinPoint jp) throws Throwable {
+        MethodSignature signature = (MethodSignature) jp.getSignature();
+        Method method = signature.getMethod();
+        Optional<DeleteMapping> deleteMappingOptional = Optional.ofNullable(method.getAnnotation(DeleteMapping.class));
+        deleteMappingOptional.ifPresent(ann -> addLogInfo("DELETE", ann.value(), method.getName()));
+        Object obj;
+        try {
+            obj = jp.proceed();
+        } catch (Exception e) {
+            addLogException(e, method);
             throw e;
         }
         return obj;
@@ -83,5 +98,12 @@ public class PatientControllerAspect {
 
     private void addLogInfo(final String requestType, String[] urls, String methodName) {
         log.info(requestType + ": " + Arrays.toString(urls) + ", execute: " + methodName);
+    }
+
+    private void addLogException(Exception exception, Method method) {
+        log.warn(exception.getClass().getSimpleName() +
+                ": " +
+                exception.getMessage() +
+                ". Method: " + method.getName());
     }
 }
